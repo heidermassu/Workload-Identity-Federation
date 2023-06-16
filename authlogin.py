36433628a01783d79e874eee09596741af653b7f
Heider Leandro Massu Hengstmann <heidermassu@gmail.com>
2023-06-16 16:42:38 -0300

    actions

--- authlogin.py
+++ authlogin.py
@@ -1,11 +1,7 @@
 from google.auth import default, exceptions
 from google.auth.transport import requests
 import subprocess
-
-# Prompt the user for variable values
-project_id = input("Enter the project ID: ")
-location = input("Enter the location: ")
-github_repository = input("Enter the GitHub repository: ")
+import argparse
 
 
 def login_to_gcp():
@@ -30,25 +26,34 @@ def login_to_gcp():
 
     # Use the credentials for further API requests or operations
 
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="GCP Terraform Deployment Script")
+    parser.add_argument("project_id", type=str, help="Project ID")
+    parser.add_argument("location", type=str, help="Location")
+    parser.add_argument("github_repository", type=str, help="GitHub repository")
+
+    args = parser.parse_args()
+
+    project_id = args.project_id
+    location = args.location
+    github_repository = args.github_repository
+
     login_to_gcp()
 
-# Change to the oidc-simple folder
-subprocess.run(["cd", ".\\oidc-simple"], shell=True, check=True)
+    # Change to the oidc-simple folder
+    subprocess.run(["cd", ".\\oidc-simple"], shell=True, check=True)
 
-# Run terraform init
-subprocess.run(["terraform", "init"], cwd=".\\oidc-simple", check=True)
+    # Run terraform init
+    subprocess.run(["terraform", "init"], cwd=".\\oidc-simple", check=True)
 
-# Run terraform apply with variable values
-#subprocess.run(["terraform", "apply", f"-var=project_id={project_id}", f"-var=location={location}", f"-var=github_repository={github_repository}"], cwd=".\\oidc-simple", check=True)
-subprocess.run(["terraform", "apply",
-        "-var",
-        f"project_id={project_id}",
-        "-var",
-        f"location={location}",
-        "-var",
-        f"github_repository={github_repository}",
-    ],
-    cwd=".\\oidc-simple",
-    check=True,
-)
+    # Run terraform apply with variable values
+    subprocess.run(
+        ["terraform", "apply",
+         "-var", f"project_id={project_id}",
+         "-var", f"location={location}",
+         "-var", f"github_repository={github_repository}"
+        ],
+        cwd=".\\oidc-simple",
+        check=True
+    )
