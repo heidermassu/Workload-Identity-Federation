11ae29bb990ad2f0dc2135dbea1bbf9a9e701c3c
Heider Leandro Massu Hengstmann <heidermassu@gmail.com>
2023-06-16 19:03:09 -0300

    actions

--- authlogin.py
+++ authlogin.py
@@ -1,6 +1,6 @@
 import subprocess
 import argparse
-
+import os
 
 def terraform():
     parser = argparse.ArgumentParser(description="GCP Terraform Deployment Script")
@@ -13,24 +13,18 @@ def terraform():
     project_id = args.project_id
     location = args.location
     github_repository = args.github_repository
-    # Change to the oidc-simple folder
-    subprocess.run(["cd", ".\\oidc-simple"], shell=True, check=True)
+
+    os.chdir("oidc-simple")
 
     # Run terraform init
-    subprocess.run(["terraform", "init"], cwd=".\\oidc-simple", check=True)
+    subprocess.run(["terraform", "init"], check=True)
 
     # Run terraform apply with variable values
-    #subprocess.run(["terraform", "apply", f"-var=project_id={project_id}", f"-var=location={location}", f"-var=github_repository={github_repository}"], cwd=".\\oidc-simple", check=True)
     subprocess.run(["terraform", "apply",
-            "-var",
-            f"project_id={project_id}",
-            "-var",
-            f"location={location}",
-            "-var",
-            f"github_repository={github_repository}",
-        ],
-        cwd=".\\oidc-simple",
-        check=True,
-    )
+        "-var", f"project_id={project_id}",
+        "-var", f"location={location}",
+        "-var", f"github_repository={github_repository}",
+    ], check=True)
+
 if __name__ == "__main__":
     terraform()
